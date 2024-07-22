4f411869fc43e50d385869cd1d41c1dd2efda312
Dylanoco <dylanoco.official@gmail.com>
2024-07-22 19:35:56 +0100

    Getting Embeds Working so the user can view clips they have created

--- react-project/src/ViewClip.jsx
+++ react-project/src/ViewClip.jsx
@@ -5,6 +5,8 @@ function ViewClip(){
     return(
     <div className="viewclip-container">
         <header>View Clip</header>
+        <iframe src="https://clips.twitch.tv/embed?clip=SillyEncouragingFalconKappaWealth-9tdJptqtiutwKRWo&parent=localhost" 
+        frameborder="0" allowfullscreen="true" scrolling="no" height="378" width="620"></iframe>
     </div>
     )
 }
