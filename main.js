6d30ff97968348c9ac3e2769c580ed67c4c6232c
Dylanoco <dylanoco.official@gmail.com>
2024-12-15 23:51:04 +0000

    changing loaded webpage to netlify

--- main.js
+++ main.js
@@ -33,7 +33,7 @@ function createWindow() {
     return { action: 'deny' };
   });
 
-  mainWindow.loadURL("http://localhost:5001/");
+  mainWindow.loadURL("https://twitchmoments.netlify.app/");
   mainWindow.webContents.on('did-fail-load', () => {
     log.error('Failed to load content.');
   });
