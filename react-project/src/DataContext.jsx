96eb8bb30b283aeb9299cc82f63f4647d3d5c360
Dylanoco <dylanoco.official@gmail.com>
2025-01-24 23:14:17 +0000

    ^^^

--- react-project/src/DataContext.jsx
+++ react-project/src/DataContext.jsx
@@ -1,7 +1,7 @@
-import React, { createContext, useState, useEffect } from 'react'
+import React, { createContext, useState, useEffect } from 'react';
 import { io } from 'socket.io-client';
 
-import {Toaster, toast} from 'sonner'
+import {Toaster, toast} from 'sonner';
 
 //DataContext / DataProvider used to help the two components communicate with clips.
 export const DataContext = createContext();
@@ -29,6 +29,7 @@ export const DataProvider = ({ children }) => {
 
     return( //Provides the two useStates to both children 
         <DataContext.Provider value={{clips, setClips, slug, setSlug }}> 
+        <Toaster />
             {children}
         </DataContext.Provider>
     )
