7108f991a465e66518e6677ebcfdb6d147df113f
Dylanoco <dylanoco.official@gmail.com>
2025-01-24 23:03:22 +0000

    ^^

--- react-project/vite.config.js
+++ react-project/vite.config.js
@@ -5,4 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()], 
   server: {port: 5001},
+  build: {
+    rollupOptions: {
+      external: ['sonner'],
+    },
+  },
 })
