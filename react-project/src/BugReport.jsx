066ec463f1ea9189a7a1e80b4a5d8855464dac78
Dylanoco <dylanoco.official@gmail.com>
2025-02-17 23:38:56 +0000

    added submit button

--- react-project/src/BugReport.jsx
+++ react-project/src/BugReport.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import styles from './BugReport.module.css';
 const BugReport = ({ isOpen, toggleModal }) => {
-    if (!isOpen) return null;
+  if (!isOpen) return null;
 
-    return (
-        <div className="modal">
-        <div onClick={toggleModal} className={styles.overlay}></div>
-        <div className={styles['modalContent']}>
-          <div className={styles['modalGuide']}>
-            <form>
-              <label>Email</label>
-              <input></input>
-              <label>Description</label>
-              <textarea></textarea>
-            </form>
-          </div>
+  return (
+    <div className="modal">
+      <div onClick={toggleModal} className={styles.overlay}></div>
+      <div className={styles['modalContent']}>
+        <div className={styles['modalGuide']}>
+          <form>
+            <label>Email</label>
+            <input></input>
+            <label>Description</label>
+            <textarea></textarea>
+            <input type="submit"></input>
+          </form>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default BugReport;
