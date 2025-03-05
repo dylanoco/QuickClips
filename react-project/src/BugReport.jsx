61f38fa870c7b8a09b860d7e945dddb55d22afac
Dylanoco <dylanoco.official@gmail.com>
2025-03-05 13:46:08 +0000

    Installing Nodemailer.

--- react-project/src/BugReport.jsx
+++ react-project/src/BugReport.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import { useState } from 'react';
 import styles from './BugReport.module.css';
 const BugReport = ({ isOpen, toggleModal }) => {
   if (!isOpen) return null;
 
 const [email, setEmail] = useState("");
 const [description, setDescription] = useState("");
+
 const handleEmailChange = (e) => setEmail(e.target.value);
 const handleDescriptionChange = (e) => setDescription(e.target.value);
+const handleSubmit = (e) =>{
+  e.PreventDefault();
+  console.log(e);
+}
 
   return (
     <div className="modal">
