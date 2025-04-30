42b951fd58c7def7326b3dcd8047e2d61ef1f80c
Dylanoco <dylanoco.official@gmail.com>
2025-04-30 14:55:55 +0100

    Routing

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -7,7 +7,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/app" element={<App />} />  
+        <Route path="app" element={<App />} />  
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
