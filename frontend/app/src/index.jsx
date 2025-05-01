e7e06c270adfe7adb3ea709655fc9879e39ad545
Dylanoco <dylanoco.official@gmail.com>
2025-05-01 11:53:23 +0100

    Routing

--- frontend/app/src/index.jsx
+++ frontend/app/src/index.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import { BrowserRouter, Routes } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="app" element={<App />} />  
+        <Route path="/" element={<App />} />  
       </Routes>
     </BrowserRouter>
-  </React.StrictMode>
-);
+  </React.StrictMode
