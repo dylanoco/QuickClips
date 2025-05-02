2eb12af51b3c46dbe525eb3490c74f7478501f0b
Dylanoco <dylanoco.official@gmail.com>
2025-05-02 10:24:10 +0100

    Renaming and Moving Folders

--- frontend/react-app/src/index.jsx
+++ frontend/react-app/src/index.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Landing from './Landing.jsx';
+// import Landing from './Landing.jsx';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/app'>
       <Routes>
-        <Route path="/app" element={<App />} />  
-        <Route path="/" element={<Landing/>} />
+        <Route path="/" element={<App />} />  
+        {/* <Route path="/" element={<Landing/>} /> */}
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
