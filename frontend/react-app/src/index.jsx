3b2e56c7780fc207f108ff1eb64143844ac7afcf
Dylanoco <dylanoco.official@gmail.com>
2025-06-28 12:19:37 +0100

    feat: add Google Analytics integration with react-ga4 for tracking

--- frontend/react-app/src/index.jsx
+++ frontend/react-app/src/index.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-S1CP97ZRKV");
 // import Landing from './Landing.jsx';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
